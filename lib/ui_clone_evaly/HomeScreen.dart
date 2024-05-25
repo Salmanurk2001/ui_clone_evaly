6bedfebb8efc627be0c987d1625c3a4130b4efed
Salmanurk2001 <salmanurk2001@gmail.com>
2024-05-25 16:31:28 +0530

    fail

--- lib/ui_clone_evaly/HomeScreen.dart
+++ lib/ui_clone_evaly/HomeScreen.dart
@@ -1,30 +1,51 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(home: HomeScreen(),
+void main() {
+  runApp(MaterialApp(
+    home: HomeScreen(),
     debugShowCheckedModeBanner: false,
-    theme: ThemeData(primarySwatch:Colors.green),));
+    theme: ThemeData(primarySwatch: Colors.green),
+  ));
 }
-class HomeScreen extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return DefaultTabController(length: 4,
-        child: Scaffold(
-            appBar: AppBar(
-              centerTitle: true,
-              backgroundColor: Colors.white,
-              title: Text("evaly"),
-              leading: Icon(Icons.menu),
-              actions: [Icon(Icons.person)],
-              bottom: TabBar(tabs: [
-                Text("Categorey",style: TextStyle(fontSize: 12.8 ),),
-                Text("Brands",style: TextStyle(fontSize: 12.8 ),),
-                Text("Shop",style: TextStyle(fontSize: 12.8 ),),
-              ]),
-            ),
 
-        )
-    );
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            backgroundColor: Colors.white,
+            title: Text("evaly"),
+            leading: Icon(Icons.menu),
+            actions: [Icon(Icons.person)],
+            bottom: TabBar(tabs: [
+              Text(
+                "Categorey",
+                style: TextStyle(fontSize: 12.8),
+              ),
+              Text(
+                "Brands",
+                style: TextStyle(fontSize: 12.8),
+              ),
+              Text(
+                "Brands",
+                style: TextStyle(fontSize: 12.8),
+              ),
+              Text(
+                "Brands",
+                style: TextStyle(fontSize: 12.8),
+              ),
+              Text(
+                "Shop",
+                style: TextStyle(fontSize: 12.8),
+              ),
+            ]),
+          ),
+        ));
   }
 }
